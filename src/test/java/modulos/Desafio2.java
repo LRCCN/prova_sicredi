0adaf81f15a962cf9eddb7dcd840e76dc884baa5
Luiz Neto <luizitow@hotmail.com>
2022-07-28 11:07:09 -0400

    Removi imports nao utilizados

--- src/test/java/modulos/Desafio2.java
+++ src/test/java/modulos/Desafio2.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
